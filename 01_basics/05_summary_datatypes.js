0852237652176655608d685d794bc1dc75c905f8
rahuldogney02 <rahuldogney02@gmail.com>
2024-12-28 18:00:20 +0000

    datatype summary

---
+++ 01_basics/05_summary_datatypes.js
@@ -0,0 +1,27 @@
+// primitive and non-primitive or refrence datatype
+// 7 Types : String, Number, Boolean, null empty means memory not alocated, undefind not-intialize means memory alocated but value not assigned, Symbole is used for making unique all values, BigInt sintific value. 
+
+// Js is Dinamic type language
+
+const outSideTemp = null;
+let userEmail;
+
+const id = Symbol('123');
+const anotherId = Symbol('123');
+console.log( id === anotherId);
+const bigNum = 3245374749384853n;
+console.log(typeof bigNum , bigNum);
+
+// In memory directly allocate refrence (non-primitive) type data type : Array, Object, Functions
+
+const  fruites = ['apple', 'mango', 'greeps', 'banana'];
+let myObj = {
+    name: "rahul",
+    age: 34
+}
+const myFunction = function(){
+    return "Hello World"  
+}
+console.table([fruites, myObj, myFunction(),`'outSideTemp = null :-' ${typeof outSideTemp}`]);
+
+// Refrence - https://262.ecma-international.org/5.1/#sec-11.4.3 typerOf
